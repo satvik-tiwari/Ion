9a368c81f3875f46d815eb78a3d6d018985bb17f
Kivtas <satviktiwarikivtas7@gmail.com>
2024-06-12 18:54:50 +0100

    bug fixed

---
+++ Ion/src/HittableList.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef HittableList_H
+#define HittableList_H
+
+#include "Hittable.h"
+
+#include <memory>
+#include <vector>
+
+class HittableList : public Hittable
+{
+public :
+
+};
+
+#endif // !HittableList_H
